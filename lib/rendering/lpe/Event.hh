d1abc52f4cbb7326f8dbf9df35b4d6e4f87902e8
Alex Fuller <boberfly@gmail.com>
2024-08-21 23:27:44 +1000

    rendering/lpe - preprocessor define fixes for MSVC, cannot have an empty input so `EVENT` and `LPE_EVENT` are now passed through

--- lib/rendering/lpe/Event.hh
+++ lib/rendering/lpe/Event.hh
@@ -6,23 +6,23 @@
 #pragma once
 
 #define LPE_EVENT_TYPE_ENUMS(NS)            \
-    NS##EVENT_TYPE_NONE,                    \
-    NS##EVENT_TYPE_CAMERA,                  \
-    NS##EVENT_TYPE_REFLECTION,              \
-    NS##EVENT_TYPE_TRANSMISSION,            \
-    NS##EVENT_TYPE_VOLUME,                  \
-    NS##EVENT_TYPE_LIGHT,                   \
-    NS##EVENT_TYPE_EMISSION,                \
-    NS##EVENT_TYPE_BACKGROUND,              \
-    NS##EVENT_TYPE_EXTRA,                   \
-    NS##EVENT_TYPE_MATERIAL
+    NS##_TYPE_NONE,                         \
+    NS##_TYPE_CAMERA,                       \
+    NS##_TYPE_REFLECTION,                   \
+    NS##_TYPE_TRANSMISSION,                 \
+    NS##_TYPE_VOLUME,                       \
+    NS##_TYPE_LIGHT,                        \
+    NS##_TYPE_EMISSION,                     \
+    NS##_TYPE_BACKGROUND,                   \
+    NS##_TYPE_EXTRA,                        \
+    NS##_TYPE_MATERIAL
 
 #define LPE_EVENT_SCATTERING_TYPE_ENUMS(NS) \
-    NS##EVENT_SCATTERING_TYPE_NONE,         \
-    NS##EVENT_SCATTERING_TYPE_DIFFUSE,      \
-    NS##EVENT_SCATTERING_TYPE_GLOSSY,       \
-    NS##EVENT_SCATTERING_TYPE_MIRROR,       \
-    NS##EVENT_SCATTERING_TYPE_STRAIGHT
-
+    NS##_SCATTERING_TYPE_NONE,              \
+    NS##_SCATTERING_TYPE_DIFFUSE,           \
+    NS##_SCATTERING_TYPE_GLOSSY,            \
+    NS##_SCATTERING_TYPE_MIRROR,            \
+    NS##_SCATTERING_TYPE_STRAIGHT
+     
 #define LPE_NO_LABEL -1
 
